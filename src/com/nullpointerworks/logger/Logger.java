37d5aac97dba5c88ac9cc5ebf7b574d0aaf765d6
Nullpointer <nullpointerworks@outlook.com>
2020-05-18 11:50:05 +0200

    update
    removing testcode from the library

--- src/com/nullpointerworks/logger/Logger.java
+++
@@ -1,7 +0,0 @@
-package com.nullpointerworks.logger;
-
-public interface Logger 
-{
-	void info(String message);
-	void error(String message);
-}
--- src/module-info.java
+++ src/module-info.java
@@ -7,4 +7,8 @@ module libnpw.jasm
 	exports com.nullpointerworks.jasm.compiler;
 	exports com.nullpointerworks.jasm.virtualmachine;
 	exports com.nullpointerworks.jasm.virtualmachine.instruction;
+	exports com.nullpointerworks.jasm.virtualmachine.instruction.arithmetic;
+	exports com.nullpointerworks.jasm.virtualmachine.instruction.controlflow;
+	exports com.nullpointerworks.jasm.virtualmachine.instruction.dataflow;
+	exports com.nullpointerworks.jasm.virtualmachine.instruction.system;
 }
